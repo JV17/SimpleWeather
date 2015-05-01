5c28fb289b1bc5150e5e1de80ad2a7c56009b704
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-05-01 16:29:22 -0400

    added custom fonts

--- LoyaltyOne/Utils/AppHelper.swift
+++ LoyaltyOne/Utils/AppHelper.swift
@@ -9,5 +9,108 @@
 import UIKit
 
 class AppHelper: NSObject {
-   
+    
+    
+    func applyBlurToView(view: UIView, withBlurEffectStyle:UIBlurEffectStyle) {
+
+        //only apply the blur if the user hasn't disabled transparency effects
+        if(!UIAccessibilityIsReduceTransparencyEnabled()) {
+            
+            let blurEffect = UIBlurEffect(style: withBlurEffectStyle)
+            let blurEffectView = UIVisualEffectView(effect: blurEffect)
+            blurEffectView.frame = view.bounds;
+            
+            view.addSubview(blurEffectView)
+            
+        }
+        else {
+            
+            view.backgroundColor = UIColor(patternImage: UIImage(named: "background1")!)
+
+        }
+    }
+    
+    
+    func reSizeBackgroundImageIfNeeded(image: UIImage, newSize: CGSize) -> UIImage {
+        
+        if(image.size.height != newSize.height || image.size.width != newSize.width) {
+
+            // resize the background image only if neeeded
+            return self.reSizeImage(image, newSize: newSize)
+        }
+
+        // we don't need to resize the image so return it
+        return image
+    }
+    
+    
+    func reSizeImage(image: UIImage, newSize: CGSize) -> UIImage {
+        
+        let image = image.CGImage
+        
+        let width = Int(newSize.width) //CGImageGetWidth(image) / 2
+        let height = Int(newSize.height) //CGImageGetHeight(image) / 2
+        let bitsPerComponent = CGImageGetBitsPerComponent(image)
+        let bytesPerRow = CGImageGetBytesPerRow(image)
+        let colorSpace = CGImageGetColorSpace(image)
+        let bitmapInfo = CGImageGetBitmapInfo(image)
+        
+        let context = CGBitmapContextCreate(nil, width, height, bitsPerComponent, bytesPerRow, colorSpace, bitmapInfo)
+        
+        CGContextSetInterpolationQuality(context, kCGInterpolationHigh)
+        
+        CGContextDrawImage(context, CGRect(origin: CGPointZero, size: CGSize(width: CGFloat(width), height: CGFloat(height))), image)
+        
+        let scaledImage = UIImage(CGImage: CGBitmapContextCreateImage(context))!
+        
+        return scaledImage
+    }
+    
+    
+    func applyGradientFromColors(colors: Array<AnyObject>, view: UIView) {
+        let gradient : CAGradientLayer = CAGradientLayer()
+        
+        gradient.frame = view.bounds
+        gradient.colors = colors
+        
+        view.layer.insertSublayer(gradient, atIndex: 0)
+    }
+    
+    
+    func colorWithHexString (hex:String) -> UIColor {
+        var cString:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet).uppercaseString
+        
+        if (cString.hasPrefix("#"))
+        {
+            cString = cString.substringFromIndex(advance(cString.startIndex, 1))
+        }
+        
+        if (count(cString) != 6)
+        {
+            return UIColor.grayColor()
+        }
+        
+        var rgbValue:UInt32 = 0
+        NSScanner(string: cString).scanHexInt(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
+    func displayFontFamilies() {
+        
+        for family in UIFont.familyNames() {
+            
+            println("\(family)")
+            
+            for name in UIFont.fontNamesForFamilyName(family as! String) {
+                println("\(name)")
+            }
+        }
+    }
+
 }
--- LoyaltyOne/ViewController.swift
+++ LoyaltyOne/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:
+    //MARK: Properties
+
+    let appHelper = AppHelper()
+    let screenSize = UIScreen.mainScreen().bounds
+    
+    //MARK:
+    //MARK: Lazy loading
+
+    lazy var blurredView: UIView = {
+        var tmpView: UIView = UIView(frame: UIScreen.mainScreen().bounds)
+        return tmpView
+    }()
+    
+    lazy var timeLabel: UILabel = {
+        var tmpLabel: UILabel = UILabel(frame: CGRectMake(0, 30, self.screenSize.width, 42))
+        return tmpLabel
+    }()
+
+    //MARK:
+    //MARK: Initializers
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background1")!)
+        // setup controller
+        self.commonInit()
         
     }
 
@@ -28,6 +50,51 @@ class ViewController: UIViewController {
         return .LightContent
     }
     
+    func commonInit() {
+        
+        // setting up the blurred background image
+        self.setupBackgroundImage()
+        
+        appHelper.displayFontFamilies()
+        
+        timeLabel.font = UIFont(name: "AppleSDGothicNeo-Thin", size: 52)
+        timeLabel.backgroundColor = UIColor.yellowColor()
+        timeLabel.textColor = UIColor.blackColor()
+        timeLabel.textAlignment = NSTextAlignment.Center
+        timeLabel.text = self.getCurrentTime()
+        
+        self.view.addSubview(self.timeLabel)
+    }
+    
+    
+    
+    //MARK:
+    //MARK: Controller helper functions
+    
+    func setupBackgroundImage() {
+       
+        // we need to check if we have a correct image size to use as our background image
+        let bgImage = appHelper.reSizeBackgroundImageIfNeeded(UIImage(named: "background1")!, newSize: self.screenSize.size)
+        
+        self.view.backgroundColor = UIColor(patternImage: bgImage)
+        
+        // creating blurred view
+        self.blurredView.backgroundColor = UIColor(patternImage: bgImage)
+        appHelper.applyBlurToView(self.blurredView, withBlurEffectStyle: .Dark)
+        self.blurredView.alpha = 0.9
+        
+        self.view.addSubview(self.blurredView)
+    }
+    
+    func getCurrentTime() -> String {
+        
+        // we get current timestamp
+        let date = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.timeStyle = .ShortStyle
+        
+        return formatter.stringFromDate(date)
+    }
     
 }
 
