5d7d009f50cd510686d9e0602bdfb08e7cf61e8e
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-05-01 15:16:39 -0400

    added background images and util class

---
+++ LoyaltyOne/Utils/AppHelper.swift
@@ -0,0 +1,13 @@
+//
+//  AppHelper.swift
+//  LoyaltyOne
+//
+//  Created by Jorge Valbuena on 2015-05-01.
+//  Copyright (c) 2015 Jorge Valbuena. All rights reserved.
+//
+
+import UIKit
+
+class AppHelper: NSObject {
+   
+}
--- LoyaltyOne/ViewController.swift
+++ LoyaltyOne/ViewController.swift
@@ -12,14 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background1")!)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
     }
 
-
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
+    
 }
 
