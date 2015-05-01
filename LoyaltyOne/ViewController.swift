6797b21a0f466512c0dfffdd6458ac94d08e1f51
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-05-01 14:01:15 -0400

    Initial Commit

---
+++ LoyaltyOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoyaltyOne
+//
+//  Created by Jorge Valbuena on 2015-05-01.
+//  Copyright (c) 2015 Jorge Valbuena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
