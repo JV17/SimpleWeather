9ccfa7569b6d2ec733adcf0286300569fb21695e
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-05-04 21:40:55 -0400

    added Reachability and bridging header

---
+++ LoyaltyOne/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  LoyaltyOne
+//
+//  Created by Jorge Valbuena on 2015-05-04.
+//  Copyright (c) 2015 Jorge Valbuena. All rights reserved.
+//
+
+#ifndef LoyaltyOne_Bridging_Header_h
+#define LoyaltyOne_Bridging_Header_h
+
+#import "Reachability.h"
+
+#endif
